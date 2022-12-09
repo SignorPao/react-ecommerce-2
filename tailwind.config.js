8a89a720b11c9f086d04c63dbdf38109b0f64eaf
Pavel Belousov <signorpao@gmail.com>
2022-12-09 12:57:20 +0300

    Settings

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      primary: ['Prompt', 'sans-serif'],
+    },
+    container: {
+      padding: {
+        default: "16px",
+        lg: "0",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1100px",
+      "2xl": "1560px",
+    },
+    extend: {
+      colors: {
+        // primary: "",
+        // secondary: "",
+      },
+      // backgroundImage: {},
+    },
+  },
+  plugins: [],
+};
