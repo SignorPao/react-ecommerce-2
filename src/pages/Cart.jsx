4a02668b8bd5c15c0048e75980462ab2364fdfb7
Pavel Belousov <signorpao@gmail.com>
2022-12-23 23:12:47 +0300

    some fix

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -20,34 +20,34 @@ const Cart = () => {
     useContext(CartContext);
 
   return (
-    <section className="pt-40 pb-12 lg:py-32 min-h-screen select-none">
+    <section className="pt-28 lg:pt-40 pb-12 lg:py-32 min-h-screen overflow-hidden select-none relative">
       {itemAmount ? (
         // cart page
         <div className="container mx-auto">
           {/* cart header */}
           <div className="flex items-center justify-between capitalize">
-            <h3 className="text-3xl font-medium">Your cart</h3>
+            <h3 className="text-xl lg:text-3xl font-medium">Your cart</h3>
             <Link to={"/#products"}>
-              <span className="text-base hover:text-accent transition duration-300">
+              <span className="text-xs lg:text-base hover:text-accent transition duration-300">
                 Continue shopping
               </span>
             </Link>
           </div>
 
           {/* cart items table */}
-          <table className="table-fixed min-w-full mt-8">
+          <table className="table-fixed min-w-full mt-4 lg:mt-8">
             <thead className="border-b-[0.5px] border-black/20">
-              <tr className="">
-                <th className="w-[60%] text-left py-4 uppercase font-extralight text-xs">
+              <tr>
+                <th className="w-[40%] lg:w-[60%] text-left py-4 uppercase font-extralight text-[10px] lg:text-xs">
                   Product
                 </th>
-                <th className="w-[10%] text-center py-4 uppercase font-extralight text-xs">
+                <th className="w-[15%] lg:w-[10%] text-center py-4 uppercase font-extralight text-[10px] lg:text-xs">
                   Price
                 </th>
-                <th className="w-[20%] text-center py-4 uppercase font-extralight text-xs">
+                <th className="w-[30%] lg:w-[20%] text-center py-4 uppercase font-extralight text-[10px] lg:text-xs">
                   Quantity
                 </th>
-                <th className="w-[10%] text-right py-4 uppercase font-extralight text-xs">
+                <th className="w-[15%] lg:w-[10%] text-right py-4 uppercase font-extralight text-[10px] lg:text-xs">
                   Total
                 </th>
               </tr>
@@ -60,30 +60,30 @@ const Cart = () => {
                 return (
                   <tr key={id} className="border-b-[0.5px] border-black/20">
                     {/* image & title */}
-                    <td className="text-left py-8 flex gap-x-12">
+                    <td className="text-left py-4 lg:py-8 flex gap-x-2 lg:gap-x-12">
                       {/* image */}
                       <Link to={`/product/${id}`}>
                         <img
                           src={image}
                           alt="product"
-                          className="max-w-[60px]"
+                          className="max-w-[40px] lg:max-w-[60px]"
                         />
                       </Link>
 
                       {/* title & remove icon */}
-                      <div className="w-full flex flex-col gap-y-2">
+                      <div className="w-full flex flex-col gap-y-1 lg:gap-y-2">
                         <Link
                           to={`/product/${id}`}
-                          className="w-fit text-sm uppercase font-medium text-black hover:underline"
+                          className="w-fit text-[10px] lg:text-sm uppercase font-medium text-black hover:underline"
                         >
                           {title}
                         </Link>
 
                         <div
-                          className="w-fit text-xl cursor-pointer"
+                          className="w-fit cursor-pointer"
                           onClick={() => removeFromCart(id)}
                         >
-                          <button className="text-[10px] uppercase py-[1px] px-4 text-black/50 hover:text-black bg-gray-100 hover:bg-red-300 transition duration-300">
+                          <button className="text-[8px] lg:text-[10px] uppercase py-1 px-4 text-black/50 hover:text-black bg-gray-100 hover:bg-red-300 transition duration-300">
                             Remove
                           </button>
                         </div>
@@ -91,15 +91,15 @@ const Cart = () => {
                     </td>
 
                     {/* price */}
-                    <td className="max-w-[10%] text-center py-8">
-                      <div className="flex-1 flex items-center justify-around text-accent">
+                    <td className="max-w-[15%] lg:max-w-[10%] text-center py-4 lg:py-8">
+                      <div className="flex-1 flex items-center justify-around text-accent text-xs lg:text-base">
                         $ {price}
                       </div>
                     </td>
 
                     {/* quantity */}
-                    <td className="max-w-[20%] py-8">
-                      <div className="flex max-w-[150px] mx-auto items-center justify-center h-full border text-black font-medium py-4">
+                    <td className="max-w-[30%] lg:max-w-[20%] py-4 lg:py-8">
+                      <div className="flex max-w-[100px] lg:max-w-[150px] mx-auto items-center justify-center h-full border text-black font-medium py-2 lg:py-4">
                         {/* minus */}
                         <div
                           className="flex-1 h-full flex items-center justify-center cursor-pointer active:scale-125 hover:text-red-500"
@@ -109,7 +109,7 @@ const Cart = () => {
                         </div>
 
                         {/* amount */}
-                        <div className="h-full flex items-center justify-center px-2">
+                        <div className="h-full flex items-center justify-center px-1 lg:px-2">
                           {amount}
                         </div>
 
@@ -124,8 +124,8 @@ const Cart = () => {
                     </td>
 
                     {/* final price */}
-                    <td className="max-w-[10%] text-right py-8">
-                      <div className="flex-1 flex justify-end items-center font-semibold text-accent">{`$ ${parseFloat(
+                    <td className="max-w-[15%] lg:max-w-[10%] text-right py-4 lg:py-8">
+                      <div className="flex-1 flex justify-end items-center font-semibold text-accent text-xs lg:text-base">{`$ ${parseFloat(
                         price * amount
                       ).toFixed(2)}`}</div>
                     </td>
@@ -136,22 +136,22 @@ const Cart = () => {
           </table>
 
           {/* cart bottom */}
-          <div className="flex justify-between pt-8 gap-x-10">
+          <div className="flex flex-col gap-y-8 lg:flex-row lg:justify-between pt-8 gap-x-10">
             {/* order notes */}
             <div className="flex-1 flex flex-col">
               <span className="text-xs">Order notes:</span>
               <textarea
                 className="border focus:outline-none p-4"
-                cols="30"
-                rows="5"
+                cols="20"
+                rows="3"
                 placeholder="Your message..."
               ></textarea>
             </div>
 
             {/* total & shipping */}
-            <div className="flex-1 flex flex-col items-end gap-y-8">
+            <div className="flex-1 flex flex-col items-center lg:items-end gap-y-8">
               {/* total */}
-              <div className="flex flex-col items-end">
+              <div className="flex flex-col items-center lg:items-end">
                 <div className="uppercase font-semibold text-accent text-xl">
                   <span className="mr-1 text-black text-base font-medium">
                     Subtotal:{" "}
@@ -177,7 +177,7 @@ const Cart = () => {
         </div>
       ) : (
         // empty cart page
-        <div className="w-full h-full flex flex-col items-center justify-center gap-y-16 capitalize">
+        <div className="w-full h-full absolute top-0 right-0 bottom-0 left-0 m-auto flex flex-col items-center justify-center gap-y-16 capitalize">
           <p className="text-xl">Your cart is empty</p>
           <Link to={"/#products"}>
             <button className="flex flex-col items-center justify-center gap-y-6 group">
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Login = () => {
   return (
-    <section className="pt-40 pb-12 lg:py-32 min-h-screen flex items-center justify-center">
+    <section className="pt-40 pb-12 lg:py-32 min-h-screen overflow-hidden flex items-center justify-center">
       Login
     </section>
   );
